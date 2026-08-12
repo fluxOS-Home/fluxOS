f9dc68763d152d022b56be14ded2af5b9666f9f7
nathen brian <vboxuser@debian.myguest.virtualbox.org>
2026-08-12 10:15:49 +0000

    added system info

--- flux/fluxctl.py
+++ flux/fluxctl.py
@@ -1,7 +1,10 @@
 import os
+import subprocess
 
 print("FluxOS Control  Utility")
 print("FluxOS is running")
+print("========================")
 print(f"Running as: {os.getlogin()}")
 print(f"Home Directory:{os.path.expanduser('~')}")
-
+print("\nSystem information:")
+subprocess.run(["uname","--a"])
